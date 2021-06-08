69b977e15cfb1f08ab173a743e3ca959e3109861
Max Korte <privat@max-korte.de>
2021-06-08 21:52:44 +0200

    initial commit

---
+++ src/main/java/com/maxkorte/tierarztpraxis/TierarztpraxisApplication.java
@@ -0,0 +1,13 @@
+package com.maxkorte.tierarztpraxis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TierarztpraxisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TierarztpraxisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maxkorte/tierarztpraxis/TierarztpraxisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxkorte.tierarztpraxis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TierarztpraxisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
